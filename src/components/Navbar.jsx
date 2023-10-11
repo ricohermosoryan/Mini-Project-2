c03e4c53377f85510b499ee67cadf0c6be722b4a
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-10-11 18:00:51 +0800

    update navbar and add Login Modal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import "../css/LoginModal.css"
 
 export default function Navbar() {
   const nav = [
@@ -7,7 +8,20 @@ export default function Navbar() {
     {name: "Product", href: "/products"},
     {name: "Reviews", href: "/reviews"},
     {name: "Blogs", href: "/blogs"}
-  ]
+  ];
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal){
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
     <>
     <div className='bg-gray-800'>
@@ -15,7 +29,11 @@ export default function Navbar() {
       <div className="row-1 flex justify-between items-center p-2 border-b lg:mx-52">
           <div><img src="./src/assets/logo.svg" className="h-16"></img></div>
           <div>Search</div>
-          <div>Cart || Login/Register</div>
+          <div className='flex'>Cart ||
+          <button onClick={toggleModal} className="block text-white" type="button">
+            Login/Register
+          </button>
+          </div>
       </div>
       <div className="row-2 flex justify-center gap-10 p-2">
         {nav.map((item , i) => (
@@ -26,7 +44,67 @@ export default function Navbar() {
       </div>
     </div>
     </div>
-    
+    {/* Login Modal */}
+    {modal && (
+      <div className="modal">
+      <div onClick={toggleModal} className='overlay'>
+      <button onClick={toggleModal} className="close-modal hover:bg-gray-600 rounded-lg" >
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+      </div>
+      <div className='modal-content '>
+        <div className="chooseAccount flex gap-4 border-b-2 bg-slate-100 justify-center">
+          <button className=' bg-blue-500 p-2 rounded-3xl mt-5 mb-5 ms-3'>Login with Facebook</button>
+          <button className=' bg-red-700 p-2 rounded-3xl mt-5 mb-5 me-3'>Login with Google</button>
+        </div>
+        <div className="input flex mt-4 ms-5 me-2 mb-5">
+          <div className="login w-1/2 border-e pe-5">
+            <span className=' text-gray-600 tracking-widest text-xl font-medium'>LOGIN</span>
+            <form>
+              <div className="mb-6 mt-4">
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 " placeholder="name@example.com" required/>
+              </div>
+              <div className="mb-6">
+                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder='********' required/>
+              </div>
+              <div className="flex items-start mb-6">
+                <div className="flex items-center h-5">
+                  <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-gray-500 focus:border-gray-500 " required/>
+                </div>
+                <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+              </div>
+              <button type="submit" className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">LOG IN</button>
+            </form>
+          </div>
+          <div className="register w-1/2 border-s ps-2">
+            <span className=' text-gray-600 tracking-widest text-xl font-medium'>REGISTER</span>
+            <form>
+              <div className="mb-6 mt-4">
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder="name@example.com" required/>
+              </div>
+              <div className="mb-6">
+                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5" placeholder='********' required/>
+              </div>
+              <div className="flex items-start mb-6">
+                <div className="flex items-center h-5">
+                  <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-gray-500 focus:border-gray-500 " required/>
+                </div>
+                <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subscribe to our TechNews</label>
+              </div>
+              <button type="submit" className="text-white bg-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">REGISTER</button>
+            </form>
+          </div>
+        </div>
+      </div>
+     </div>
+    )}
     </>
   )
 }
