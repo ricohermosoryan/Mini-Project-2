b62750460118a09ad90e771a7a1932ad278a6faa
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-10-16 13:17:22 +0800

    update footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export default function Footer() {
   return (
@@ -9,11 +10,7 @@ export default function Footer() {
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <Link to="*">
-                <img
-                  src="./src/assets/logo.svg"
-                  className=" h-40 mr-3 "
-                  alt="image"
-                />
+                <img src={logo} className=" h-40 mr-3 " alt="image" />
               </Link>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
