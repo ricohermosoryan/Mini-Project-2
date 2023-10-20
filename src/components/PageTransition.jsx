fb42b6374bd95ef586b11a97d6cccbf7eb9392a9
Ryan Christopher Ricohermoso <ricohermosoryan14344@gmail.com>
2023-10-20 17:58:32 +0800

    update animation on review page

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -16,7 +16,7 @@ export default function PageTransition({ children }) {
     <motion.div
       transition={{
         ease: "easeInOut",
-        duration: 0.4,
+        duration: 0.6,
       }}
       variants={Animation}
       initial="initial"
--- src/components/pages/Reviews.jsx
+++ src/components/pages/Reviews.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import reviews from "../../assets/reviews.svg";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import PageTransition from "../PageTransition";
 
 export default function Reviews() {
-
   const [data, setData] = useState([]);
-  const [displayedReviews, setDisplayedReviews] = useState([]); 
+  const [displayedReviews, setDisplayedReviews] = useState([]);
   const [reviewsToShow, setReviewsToShow] = useState(21);
   const [users, setUsers] = useState([]);
   const [products, setProducts] = useState([]);
@@ -17,25 +16,30 @@ export default function Reviews() {
     setLoading(true);
 
     async function fetchData() {
-      const usersRes = await fetch('https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/users');
+      const usersRes = await fetch(
+        "https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/users"
+      );
       const userData = await usersRes.json();
       setUsers(userData);
 
-      const productsRes = await fetch('https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/products');
-      const productData = await productsRes.json(); 
+      const productsRes = await fetch(
+        "https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/products"
+      );
+      const productData = await productsRes.json();
       setProducts(productData);
 
-      const reviewsRes = await axios.get("https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/reviews");
+      const reviewsRes = await axios.get(
+        "https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/reviews"
+      );
       const reviewsData = reviewsRes.data;
       setData(reviewsData);
     }
 
     fetchData()
-      .catch(err => console.error(err))
+      .catch((err) => console.error(err))
       .finally(() => setLoading(false));
 
     setDisplayedReviews(data.slice(0, reviewsToShow));
-
   }, [data, reviewsToShow]);
 
   // Rating icon
@@ -45,35 +49,55 @@ export default function Reviews() {
       case 1:
         icons = (
           <>
-            <i className='bx bxs-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
           </>
         );
         break;
       case 2:
         icons = (
           <>
-            <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
           </>
         );
         break;
       case 3:
         icons = (
           <>
-            <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bx-star'></i><i className='bx bx-star'></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bx-star"></i>
+            <i className="bx bx-star"></i>
           </>
         );
         break;
       case 4:
         icons = (
           <>
-            <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bx-star'></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bx-star"></i>
           </>
         );
         break;
       case 5:
         icons = (
           <>
-            <i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i><i className='bx bxs-star'></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
+            <i className="bx bxs-star"></i>
           </>
         );
         break;
@@ -84,6 +108,7 @@ export default function Reviews() {
   return (
     <>
       <PageTransition>
+        {/* Banner */}
         <div className="banner">
           <img
             src={reviews}
@@ -91,6 +116,8 @@ export default function Reviews() {
             className="lg:w-screen lg:h-[400px] lg:object-cover"
           />
         </div>
+
+        {/* Customer Reviwes Header */}
         <div className=" mx-5 lg:mx-24 mt-10 md:mt-14 lg:mt-[90px]">
           <div className=" h-[0px]  shadow border border-zinc-500"></div>
           <div className="w-[139px] h-[32.66px] px-[92px] py-[7px]  bg-neutral-100 border border-zinc-500 mt-[-18px] ms-[77px] md:ms-[238px] md:w-[240px] md:h-[40px] md:mt-[-20px] lg:ms-[740px] lg:w-[300px] lg:h-[50px] lg:mt-[-26px]">
@@ -98,38 +125,62 @@ export default function Reviews() {
               Customer Reviews
             </div>
           </div>
-          <div className="container mx-auto">
-            <div className="flex flex-wrap flex-row justify-center my-10 gap-4">
-              {displayedReviews.map(review => {
-                // Find user
-                const user = users.find(u => u.id === review.userId);
 
-                // Find product 
-                const product = products.find(p => p.id === review.productId);
-                            
-                return (
-                  <div key={review.id} className="review flex shadow w-96 h-auto p-2">
-                    <div className="min-w-fit mr-2">
-                      <img src={user.image} className="w-12 aspect-square rounded-full shadow"/>
-                    </div>
-                    <div className="">
-                      <p className="heading font-medium">{user.fullName}</p>
-                      <p className="text-sm">{product.title}</p>
-                      <p className="text-dark-quantum">{getRatingIcons(review.rating)}</p>
-                      <p className="italic">"{review.comment}"</p>
-                    </div>
-                  </div>
-                )
-              })}
+          {/* User Reviews */}
+          <AnimatePresence>
+            <div className="container mx-auto">
+              <div className="flex flex-wrap flex-row justify-center my-10 gap-4">
+                {displayedReviews.map((review) => {
+                  // Find user
+                  const user = users.find((u) => u.id === review.userId);
+
+                  // Find product
+                  const product = products.find(
+                    (p) => p.id === review.productId
+                  );
+
+                  return (
+                    <motion.div
+                      key={review.id}
+                      className="review flex shadow w-96 h-auto p-2"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      transition={{
+                        ease: "easeInOut",
+                        duration: 1,
+                      }}
+                    >
+                      <div className="min-w-fit mr-2">
+                        <img
+                          src={user.image}
+                          className="w-12 aspect-square rounded-full shadow"
+                        />
+                      </div>
+                      <div className="">
+                        <p className="heading font-medium">{user.fullName}</p>
+                        <p className="text-sm">{product.title}</p>
+                        <p className="text-dark-quantum">
+                          {getRatingIcons(review.rating)}
+                        </p>
+                        <p className="italic">"{review.comment}"</p>
+                      </div>
+                    </motion.div>
+                  );
+                })}
+              </div>
+              <div className="flex justify-center">
+                {displayedReviews.length < data.length && (
+                  <button
+                    className="text-quantum hover:text-dark-quantum"
+                    onClick={() => setReviewsToShow((prev) => prev + 21)}
+                  >
+                    Show More
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="flex justify-center">
-              {displayedReviews.length < data.length && (
-                <button className="text-quantum hover:text-dark-quantum" onClick={() => setReviewsToShow(prev => prev + 21)}>
-                  Show More
-                </button>
-              )}
-            </div>
-          </div>
+          </AnimatePresence>
         </div>
       </PageTransition>
     </>
