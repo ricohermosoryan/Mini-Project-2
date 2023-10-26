bee144bf62663178df149873820336a31676c16c
RexPacificusVII <110966402+RexPacificusVII@users.noreply.github.com>
2023-10-26 17:34:52 +0800

    Add category filter

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,9 +1,11 @@
+'use client'
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import PageTransition from "../PageTransition";
-import { BsEyeFill } from "react-icons/bs";
+import { Breadcrumb } from 'flowbite-react';
 import cart from "../../assets/cart.svg";
 import star from "../../assets/star.svg";
 import { useContext } from "react";
@@ -21,9 +23,24 @@ export default function Products() {
 
   const [data, setData] = useState([]);
 
+  const [filters, setFilters] = useState({
+    brand: "",
+    maxPrice: "",
+    category: [],
+    subcategory: "",
+    minRating: "",
+  });
+
+  const [isAccordion1Open, setAccordion1Open] = useState(false);
+  const [isAccordion2Open, setAccordion2Open] = useState(false);
+
+  const [hoveredItem, setHoveredItem] = useState(null);
+
   const [currentPage, setCurrentPage] = useState(1);
 
   const itemsPerPage = 24;
+
+  const [showFilter, setShowFilter] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -51,6 +68,36 @@ export default function Products() {
     setCurrentPage(page);
   };
 
+  const handleCategoryCheckbox = (category) => {
+    const updatedFilters = { ...filters };
+    if (updatedFilters.category.includes(category)) {
+      updatedFilters.category = updatedFilters.category.filter((c) => c !== category);
+    } else {
+      updatedFilters.category = [...updatedFilters.category, category];
+    }
+    setFilters(updatedFilters);
+  };
+
+  const applyFilters = () => {
+    const selectedCategories = filters.category;
+    axios
+      .get("https://w266v3hoea.execute-api.ap-southeast-2.amazonaws.com/dev/products/filter", {
+        params: {
+          ...filters,
+          category: selectedCategories.join(","),
+        },
+      })
+      .then((res) => {
+        const filteredData = res.data.filter(product => {
+          return selectedCategories.every(category => {
+            return product.category.includes(category);  
+          });
+        });
+        setData(filteredData);
+      })
+      .catch((err) => console.error(err));
+  };
+
   // For Page number pagination
   function generatePageNumbers(totalPages) {
     const pageNumbers = [];
@@ -61,130 +108,247 @@ export default function Products() {
   }
 
   const pageNumbers = generatePageNumbers(totalPages);
+
   return (
     <>
       <PageTransition>
-        <div className=" rounded-md mt-10 flex lg:mx-[150px]">
-          {/* Filter */}
-          <motion.div
-            className=" w-[500px] mt-5 "
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{
-              ease: "easeInOut",
-              duration: 1,
-            }}
+        <div className="container mx-auto px-4">
+
+          {/* BREADCRUMB */}
+          <div className="my-6">
+            <Breadcrumb>
+              <Breadcrumb.Item href="/home">
+                  Home
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                Products
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+
+          {/* <div>
+            <button onClick={() => setShowFilter(!showFilter)}>Filter</button>
+          </div> */}
+
+          {/* Off-canvas filter */}
+          {/* <div
+            className={`off-canvas fixed inset-y-0 left-0 w-64 bg-white shadow-lg transform z-10 ${
+              showFilter ? "translate-x-0" : "-translate-x-full"
+            } transition-transform ease-in-out duration-300`}
           >
-            <h3 className="mx-3">Filter</h3>
-            <div className="h-[1px] border border-gray-400 mx-3"></div>
-          </motion.div>
+          </div> */}
 
-          {/* Products */}
-          <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:me-[40px]">
-            {paginatedData.map((item, i) => (
-              // Products Details
-              <motion.div
-                key={i}
-                className=" aspect-square m-3 group transition relative"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{
-                  ease: "easeInOut",
-                  duration: 1,
-                }}
-              >
-                {/* Image */}
-                <div className=" flex gap-3 items-center">
-                  <img
-                    src={item.image[0]}
-                    alt="image"
-                    className=" mx-auto my-1 group-hover:scale-110 transition duration-200 mb-3 pt-8 mt-[30px] shadow-lg  "
-                  />
-                </div>
-
-                {/* Buttons */}
-                <div className="absolute bottom-0 left-[-150px] group-hover:left-0 border-sky-300 rounded-lg border-2 opacity-0 group-hover:opacity-100 transition-all duration-200">
-                  <button
-                    className="flex gap-2 p-3 font-bold"
-                    onClick={() =>
-                      addToCart({
-                        title: item.title,
-                        price: item.price,
-                        image: item.image[0],
-                        quantity: 1,
-                      })
-                    }
-                  >
-                    <img src={cart} alt="image" /> Add to Cart
-                  </button>
-                </div>
-
-                {/* View */}
-                <div className=" bg-sky-300/40 w-[50px] h-[50px] rounded-lg absolute top-[-60px] right-0 group-hover:top-0 opacity-0 group-hover:opacity-100 flex items-center justify-end transition-all duration-200 lg:top-[50px] lg:right-[-50px] lg:group-hover:right-[-16px] lg:group-hover:top-[50px]">
-                  <Link
-                    to={`${item.id}`}
-                    className="w-[35px] h-[35px] bg-white flex items-center justify-center me-2 rounded-md"
-                  >
-                    <BsEyeFill className="w-[20px] h-[20px]" />
-                  </Link>
-                </div>
-
-                {/* Product Details */}
-                <div className="mx-4 lg:h-[150px]">
-                  <p className="truncate mx-3 mt-[10px] text-[18px] font-semibold">
-                    {item.title}
-                  </p>
-                  <p className=" mb-[60px] mx-3 mt-2 text-xs md:text-lg">
-                    {item.category}
-                  </p>
-                  <p className="mx-3 group-hover:hidden text-left transition-all duration-200 text-[17px] font-semibold">
-                    {formatter.format(item.price)}
-                  </p>
-
-                  {/* Ratings */}
-                  <div className="border bg-quantum rounded-lg flex gap-0 px-4 w-[55px] h-[35px] items-center justify-center absolute bottom-[-40px] left-4 group-hover:hidden md:bottom-0 md:left-[170px] lg:left-[260px]">
-                    <img src={star} alt="image" className="w-[20px] h-[20px]" />
-
-                    <p className="text-white font-bold">{item.rating.rate}</p>
+          <div className="flex">
+            {/* FILTER */}
+            <div className="lg:w-1/5 xl:w-1/6 hidden lg:block">
+              <div className="flex justify-between items-baseline my-4">
+                <p className="font-bold">Filter</p>
+                <button className="text-quantum text-xs">Clear all</button>
+              </div>
+              <div className="accordion">
+                <div className="accordion-item">
+                  <div className="accordion-trigger cursor-pointer" onClick={() => setAccordion1Open(!isAccordion1Open)}>
+                    Category
+                  </div>
+                  <div className={`accordion-content ${isAccordion1Open ? "hidden" : ""}`}>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="mobile"
+                         checked={filters.category.includes("Smartphones and Accessories")}
+                         onChange={() =>
+                           handleCategoryCheckbox("Smartphones and Accessories")
+                         }
+                       />
+                       <label for="mobile">Mobile Phones</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="laptops"
+                         checked={filters.category.includes("Laptops and Computers")}
+                         onChange={() => handleCategoryCheckbox("Laptops and Computers")}
+                       />
+                       <label for="laptops">Laptops & Computers</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="wearables"
+                         checked={filters.category.includes("Wearable Technology")}
+                         onChange={() => handleCategoryCheckbox("Wearable Technology")}
+                       />
+                       <label for="wearables">Wearables</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="audio"
+                         checked={filters.category.includes("Audio and Headphones")}
+                         onChange={() => handleCategoryCheckbox("Audio and Headphones")}
+                       />
+                       <label for="audio">Audio</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="cameras"
+                         checked={filters.category.includes("Camera and Photography")}
+                         onChange={() => handleCategoryCheckbox("Camera and Photography")}
+                       />
+                       <label for="cameras">Cameras</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="gaming"
+                         checked={filters.category.includes("Gaming Gear")}
+                         onChange={() => handleCategoryCheckbox("Gaming Gear")}
+                       />
+                       <label for="gaming">Gaming</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="smarthome"
+                         checked={filters.category.includes("Home Automation")}
+                         onChange={() => handleCategoryCheckbox("Home Automation")}
+                       />
+                       <label for="smarthome">Home Automation</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="software"
+                         checked={filters.category.includes("Software and Apps")}
+                         onChange={() => handleCategoryCheckbox("Software and Apps")}
+                       />
+                       <label for="software">Software & Apps</label>
+                     </div>
+                     <div className="flex items-center gap-2">
+                       <input
+                         type="checkbox"
+                         id="tech4kids"
+                         checked={filters.category.includes("Tech for Kids")}
+                         onChange={() => handleCategoryCheckbox("Tech for Kids")}
+                       />
+                       <label for="tech4kids">Tech for Kids</label>
+                     </div>
                   </div>
                 </div>
-              </motion.div>
-            ))}
+              </div>
+              <button onClick={applyFilters}>Apply Filters</button>
+
+            </div>
+
+            {/* PRODUCTS */}
+            <div className="w-full lg:w-4/5 xl:w-5/6 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+              {/* PRODUCT ITEMS */}
+
+              {paginatedData.map((item, i) => (
+              
+                <motion.div
+                  key={i}
+                  className="aspect-square m-3 group transition relative"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{
+                    ease: "easeInOut",
+                    duration: 1,
+                  }}
+                  onMouseEnter={() => setHoveredItem(i)} onMouseLeave={() => setHoveredItem(null)}
+                >
+                  <div className="p-4">
+                    <Link to={`${item.id}`}>
+                      <div className="relative">
+                        <img src={item.image[0]} className="absolute inset-0 rounded-lg"
+                          style={{
+                            transform: hoveredItem === i ? 'scale(1.1)' : 'scale(1)',
+                            transition: 'transform 0.5s ease-in',
+                          }}/>
+                        <img
+                          className="rounded-lg shadow"
+                          src={hoveredItem === i ? item.image[1] : item.image[0]}
+                          alt={item.title}
+                          style={{
+                            transform: hoveredItem === i ? 'scale(1.1)' : 'scale(1)',
+                            opacity: hoveredItem === i ? 1 : 0.8,
+                            transition: 'transform 0.5s ease-in, opacity 0.3s ease-in',
+                          }}
+                        />
+                      </div>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to={`${item.id}`}>
+                      <p className="truncate heading font-medium">{item.title}</p>
+                    </Link>
+                    <p className="text-sm text-dark-quantum mb-2">{item.brand}</p>
+                    <div className="flex items-center justify-between py-2 opacity-100 group-hover:opacity-0 transition-all duration-200">
+                      <p className="font-semibold">{formatter.format(item.price)}</p>
+                      <div className="bg-quantum flex gap-1 py-1 px-2 rounded-md">
+                        <img src={star} alt="image" className="w-5 h-5" />
+                        <p className="text-white font-bold">{item.rating.rate}</p>
+                      </div>
+                    </div>
+
+                    {/* ADD TO CART */}
+                    <div className="absolute bottom-0 left-[-150px] group-hover:left-0 border-sky-300 rounded-lg border-2 opacity-0 group-hover:opacity-100 transition-all duration-200">
+                      <button
+                        className="flex gap-2 p-3 font-semibold"
+                        onClick={() =>
+                          addToCart({
+                            title: item.title,
+                            price: item.price,
+                            image: item.image[0],
+                            quantity: 1,
+                          })
+                        }
+                      >
+                        <img src={cart} alt="image" /> Add to Cart
+                      </button>
+                    </div>
+                  </div>
+                </motion.div>
+              ))}
+            </div>
           </div>
+
+          <div className=" mt-[90px] flex gap-2 justify-center">
+            {canGoPrevious && (
+              <button
+                onClick={() => handleChangePage(currentPage - 1)}
+                className="border-2 border-gray-400 rounded-full px-3 py-2 hover:bg-black hover:text-white"
+              >
+                <FiArrowLeft />
+              </button>
+            )}
+            {pageNumbers.map((number) => (
+              <button
+                key={number}
+                onClick={() => handleChangePage(number)}
+                className={
+                  number === currentPage
+                    ? "px-4 py-2 bg-black text-white rounded-full border-2"
+                    : "border-gray-400 px-4 py-2 hover:bg-black hover:text-white rounded-full border-2 "
+                }
+              >
+                {number}
+              </button>
+            ))}
+            {canGoNext && (
+              <button
+                onClick={() => handleChangePage(currentPage + 1)}
+                className="border-2 border-gray-400 rounded-full px-3 py-2 hover:bg-black hover:text-white"
+              >
+                <FiArrowRight />
+              </button>
+            )}
+          </div>
+
         </div>
-        <div className=" mt-[90px] flex gap-2 justify-center">
-          {canGoPrevious && (
-            <button
-              onClick={() => handleChangePage(currentPage - 1)}
-              className="border-2 border-gray-400 rounded-full px-3 py-2 hover:bg-black hover:text-white"
-            >
-              <FiArrowLeft />
-            </button>
-          )}
-          {pageNumbers.map((number) => (
-            <button
-              key={number}
-              onClick={() => handleChangePage(number)}
-              className={
-                number === currentPage
-                  ? "px-4 py-2 bg-black text-white rounded-full border-2"
-                  : "border-gray-400 px-4 py-2 hover:bg-black hover:text-white rounded-full border-2 "
-              }
-            >
-              {number}
-            </button>
-          ))}
-          {canGoNext && (
-            <button
-              onClick={() => handleChangePage(currentPage + 1)}
-              className="border-2 border-gray-400 rounded-full px-3 py-2 hover:bg-black hover:text-white"
-            >
-              <FiArrowRight />
-            </button>
-          )}
-        </div>
+
       </PageTransition>
     </>
   );
