d88a3b32e5ffd578cb0496b02fdf591263e118c6
RexPacificusVII <110966402+RexPacificusVII@users.noreply.github.com>
2023-11-04 19:48:22 +0800

    Change About to  About Us

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -60,7 +60,7 @@ const DropdownMenu = ({ isOpen, toggleDropdown, dropdownType }) => {
                     to="/about"
                     className="hover:text-quantum hover:border-b hover:border-quantum"
                   >
-                    About
+                    About Us
                   </Link>
                 </li>
                 <li className="p-2 ">
