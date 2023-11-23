0a3db56e7ee407dbc9e041eb8e683c9f125e8c63
RexPacificusVII <110966402+RexPacificusVII@users.noreply.github.com>
2023-11-23 11:33:18 +0800

    Update Cart functions for transactions

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -63,6 +63,17 @@ export default function Cart() {
     fetchProductDetails();
   }, [items]);
 
+  const handleRemoveFromCart = (index) => {
+    // Call removeFromCart function
+    removeFromCart(index);
+
+    // Check if it's the last item in the cart
+    if (items.length === 1) {
+      // Reset productDetails state to an empty array when the last item is removed
+      resetProductDetails(setProductDetails);
+    }
+  };
+
   const handleRemoveAllFromCart = () => {
     removeAllFromCart();
     setProductDetails([]); // Reset productDetails state to an empty array when all items are removed
@@ -77,6 +88,78 @@ export default function Cart() {
       }
       return total;
     }, 0);
+  };
+
+  const [checkoutLoading, setCheckoutLoading] = useState(false);
+
+  const handleCheckout = async () => {
+    try {
+      setCheckoutLoading(true);
+
+      // Remove any existing transaction from localStorage
+      localStorage.removeItem('transaction');
+
+      const user_id = localStorage.getItem("_id"); // Get user ID from localStorage
+
+      const cartItems = JSON.parse(localStorage.getItem("cart")) || []; // Get cart items from localStorage
+
+      const products = cartItems.map(item => ({
+        product_id: item.product,
+        quantity: item.quantity,
+      }));
+
+      // Get unit price from already fetched product details
+      const transactionDetails = products.map((product) => {
+        const foundProduct = productDetails.find((p) => p._id === product.product_id);
+
+        if (foundProduct) {
+          return {
+            ...product,
+            unit_price: foundProduct.price - (foundProduct.price * foundProduct.discount),
+          };
+        }
+
+        throw new Error('Product details not found.'); // Handle if product details are missing
+      });
+
+      // Calculate total amount
+      const total_amount = transactionDetails.reduce((total, item) => total + (item.unit_price * item.quantity), 0);
+
+      const requestBody = {
+        type: 'purchase',
+        user_id,
+        products: transactionDetails,
+        total_amount,
+      };
+
+      // Save the full request on localStorage as 'transaction'
+      localStorage.setItem('transaction', JSON.stringify(requestBody));
+
+      // Send the request to the backend API
+      const response = await fetch('https://cupmvawskf.execute-api.ap-southeast-2.amazonaws.com/transactions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (response.ok) {
+        // Handle successful transaction creation
+        // For example, you may want to clear the cart or navigate to a thank you page
+        handleRemoveAllFromCart(); // Clear the cart after successful checkout
+        // Optionally perform other actions upon successful checkout
+      } else {
+        // Handle unsuccessful transaction creation
+        // For example, display an error message to the user
+      }
+
+    } catch (error) {
+      console.error('Error during checkout:', error);
+      // Handle error - Display an error message or perform necessary actions
+    } finally {
+      setCheckoutLoading(false);
+    }
   };
 
   return (
@@ -124,7 +207,7 @@ export default function Cart() {
                           <p className="font-semibold">{formatter.format(product.price-(product.price*product.discount))}</p>
                         </div>
                         <div className=" flex justify-start gap-3">
-                          <button onClick={() => removeFromCart(index)}>
+                          <button onClick={() => handleRemoveFromCart(index)}>
                             <img
                               src={trash}
                               alt="image"
@@ -164,8 +247,12 @@ export default function Cart() {
               </div>
               <div className="w-full flex flex-wrap gap-y-2 lg:w-1/2 grow px-8 ">
                 <Link to={`/checkout?order=${btoa(calculateTotal() * 100)}`} className="w-full grow md:w-2/5 mx-2 border rounded-lg hover:bg-dark-quantum text-white px-4 py-3 text-center heading bg-quantum">
-                  <button className="">
-                    Checkout
+                  <button
+                    className=""
+                    onClick={handleCheckout}
+                    disabled={checkoutLoading || productDetails.length === 0}
+                  >
+                    {checkoutLoading ? 'Processing...' : 'Checkout'}
                   </button>
                 </Link>
                 <button
--- src/components/pages/Checkout.jsx
+++ src/components/pages/Checkout.jsx
@@ -14,6 +14,7 @@ const Checkout = () => {
 
   const [error, setError] = useState(null);
   const [checkoutUrl, setCheckoutUrl] = useState(null);
+  const [transactionId, setTransactionId] = useState(null); // Added state for transaction ID
 
   useEffect(() => {
     const createPaymentLink = async () => {
@@ -23,7 +24,7 @@ const Checkout = () => {
             attributes: {
               amount: Number(totalAmount),
               currency: 'PHP',
-              description: 'Payment for your order',
+              description: `Payment for your order${transactionId ? ` #${transactionId}` : ''}`, // Use transaction ID in description if available
             },
           },
         };
@@ -63,7 +64,59 @@ const Checkout = () => {
     };
 
     createPaymentLink();
-  }, [totalAmount]);
+  }, [totalAmount, transactionId]); // Added transactionId as a dependency
+
+  // Retrieve the transaction from local storage
+  const localTransaction = JSON.parse(localStorage.getItem('transaction'));
+
+  // Function to fetch transactions from the backend API
+  const fetchLatestTransaction = async () => {
+    try {
+      const response = await fetch('https://cupmvawskf.execute-api.ap-southeast-2.amazonaws.com/transactions');
+      if (response.ok) {
+        const responseData = await response.json();
+
+        // Access the "transactions" property to get the array of transactions
+        const transactions = responseData.transactions;
+
+        // Ensure transactions is an array before further processing
+        if (Array.isArray(transactions)) {
+          // Sort transactions by transaction_date in descending order (latest first)
+          transactions.sort((a, b) => new Date(b.transaction_date) - new Date(a.transaction_date));
+
+          // Find the matching transaction based on user_id, products, and total_amount
+          const matchingTransaction = transactions.find((transaction) => {
+            return (
+              transaction.user_id === localTransaction.user_id &&
+              transaction.total_amount === localTransaction.total_amount
+            );
+          });
+
+          if (matchingTransaction) {
+            const transactionID = matchingTransaction._id; // Assuming _id is the transaction ID
+            setTransactionId(transactionID); // Set the retrieved transaction ID to state
+            console.log('Latest Matching Transaction ID:', transactionID);
+            return transactionID;
+          } else {
+            throw new Error('No matching transaction found');
+          }
+        } else {
+          throw new Error('Response data is not an array');
+        }
+      } else {
+        throw new Error('Failed to fetch transactions');
+      }
+    } catch (error) {
+      console.error('Error fetching or finding matching transaction:', error);
+      // Handle error - Display an error message or perform necessary actions
+      throw error;
+    }
+  };
+
+  // Call the function to fetch the latest transaction ID
+  useEffect(() => {
+    fetchLatestTransaction();
+  }, []); // Runs once on component mount
 
   return (
     <div>
